b59631f5f3b222bc4db6854c3be93e2667780f64
Wei Lu <luwei.here@gmail.com>
2014-03-21 12:16:14 +0800

    extract downloads method from cli

--- lib/github/release/stats.rb
+++ lib/github/release/stats.rb
@@ -1,10 +1,24 @@
 require_relative "stats/version"
-require_relative "stats/cli"
+require "open-uri"
+require "json"
 
 module Github
   module Release
     module Stats
-      # Your code goes here...
+      def self.downloads(repo)
+        url = "https://api.github.com/repos/#{repo}/releases"
+        releases = JSON.parse open(url).read
+
+        prod_releases =  releases.select do |r|
+          !r["draft"] && !r["prerelease"] && r["assets"] && !r["assets"].empty?
+        end
+
+        stats = prod_releases.map do |r|
+          [r["tag_name"], r["assets"].first["download_count"]]
+        end
+
+        stats
+      end
     end
   end
 end
